2ac21abf00a97f30f895948f9fd83b4a7adbe4ac
Ashwin Dotwal <ashwindotwal09@gmail.com>
2025-04-15 18:08:46 +0530

    add some code

---
+++ 8.promises/index.js
@@ -0,0 +1,25 @@
+const { error } = require("console");
+const { resolve: pathResolve, resolve } = require("path");
+
+function delayFn(time){
+  return new Promise((resolve)=>setTimeout(resolve,time));
+}
+console.log('promise lecture starts');
+
+delayFn(2000).then(()=>console.log('after 2 seconds promise resolve'));
+ console.log("end");
+
+
+ function divideFn(num1,num2){
+  return new Promise((resolve,reject)=>{
+    if(num2===0){
+      reject('can not perform division by 0')
+
+    } 
+    else{
+      resolve(num1/num2)
+    }
+  })
+ }
+
+ divideFn(10,5).then(result=>console.log(result,"res")).catch(error=> console.log(error,"err"));
