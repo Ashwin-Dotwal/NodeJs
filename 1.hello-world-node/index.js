254dee08c5433bd3636b8a59799d01d5381bfd81
Ashwin Dotwal <ashwindotwal09@gmail.com>
2025-05-19 17:19:23 +0530

    add code in node js

--- 1.hello-world-node/index.js
+++ 1.hello-world-node/index.js
@@ -1,1 +1,37 @@
+const { log } = require("console");
+
 console.log("hello jii");
+
+let a=10;
+let b=20;
+
+console.log(a+b);
+
+function greet (){
+  console.log("greet");
+
+}
+greet();
+
+
+//function are first class
+
+function logGreeting(fn){
+  fn();
+}
+
+logGreeting(greet);
+
+
+//function expration 
+
+const fn =function(){
+  console.log("Function EXpretion 1");
+}
+fn();
+
+//use function expretion on the fly
+
+logGreeting(function(){
+  console.log("function expretion 2");
+})
