4536247f157fee1d54cf76622d49b8dde18a15fa
Ashwin Dotwal <ashwindotwal09@gmail.com>
2025-05-21 00:06:17 +0530

    add some code

---
+++ serverHTML/app.js
@@ -0,0 +1,14 @@
+const http = require("http");
+
+const fs = require("fs");
+
+const server = http.createServer((req,res)=>{
+  res.writeHead(200,{'content-Type':"text/html"});
+
+ const htmlContent = fs.readFileSync(__dirname+"/index.html");
+
+
+  res.end(htmlContent)
+});
+
+server.listen(3000);
